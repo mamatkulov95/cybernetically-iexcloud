5d1c8cb34d9dc8411d18792220950dca4f1ee0f6
Ma'rufjon <m.mamatkulov95@gmail.com>
2023-08-06 00:48:50 +0500

    [Header-Menus] -Developed header menus

---
+++ src/Components/Contact/Contact.tsx
@@ -0,0 +1,9 @@
+export default function Contact(): JSX.Element {
+  return (
+    <div className="flex flex-col gap-3 items-center p-10">
+      <h2>Write e-mail. We will contact you soon!</h2>
+
+      <address className="text-gray-800">cybernetically.hr@gmail.com</address>
+    </div>
+  );
+}
